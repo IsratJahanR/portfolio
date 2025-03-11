3a23c6bf09311b2707da94dd1bcb8f773d2d49d4
IsratJahanR <isratjahan151q@gmail.com>
2025-03-11 14:28:39 +0600

    add build.sh file

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Creating staticfiles_build directory..."
-mkdir -p staticfiles_build
-cp -r staticfiles/* staticfiles_build/
+pip install -r requirements.txt
+python manage.py collectstatic 
